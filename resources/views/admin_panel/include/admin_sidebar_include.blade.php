3c2b965907c06417d03ae196df8878a7445019ff
Kashan Shaikh <shaikhkashan670@gmail.com>
2025-02-12 21:38:47 +0500

    working

---
+++ resources/views/admin_panel/include/admin_sidebar_include.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="active">
+                    <a href="#"><img src="assets/img/icons/dashboard.svg" alt="img"><span> Dashboard</span> </a>
+                </li>
+                <li class="submenu">
+                    <a href="javascript:void(0);"><img src="assets/img/icons/product.svg" alt="img"><span> Product</span> <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li><a href="#">Product List</a></li>
+                        <li><a href="#">Add Product</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="components.html"><i data-feather="layers"></i><span> Components</span> </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
